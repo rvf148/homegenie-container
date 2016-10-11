b4eab4fa5d33226ed4b42bd639824a44d5cccc52
RVHAJ03 <RVHAJ03@WP2249>
2016-10-11 20:19:12 +0200

    fixed multiline install

--- linux/Build.ps1
+++ linux/Build.ps1
@@ -1,1 +1,3 @@
 docker build -t rafvanhoof/homegenie-linux:latest --file ./Dockerfile D:\Source\GITHOMEGENIECONTAINER\homegenie-container\linux
+Write-Host "Press any key to continue ..."
+$x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
